7c52cc7e57e0355b011f0e0870cfd2c006a76df8
Armando Meza <amezal01@hotmail.com>
2024-06-08 21:57:23 -0600

    disable netrw

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -93,6 +93,8 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
