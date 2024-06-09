fe1df7dcbae563baaf756cfa3ef4600e978ed707
Armando Meza <amezal01@hotmail.com>
2024-06-08 21:29:05 -0600

    (nvim) fix NvimTreeToggle typo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -190,7 +190,7 @@ vim.keymap.set('n', '<A-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<A-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<A-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '<C-b>', '<cmd>NvimTreetoggle<CR>', { desc = 'Toggle NvimTree' })
+vim.keymap.set('n', '<C-b>', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
